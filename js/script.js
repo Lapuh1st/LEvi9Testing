0d6e2b01e0d56c7ee0148687cf891509e6a11110
Iaroslav Lopushanskyi <i.lopushanskyi@levi9.com>
2016-01-14 11:55:00 +0200

    fix bugs

--- js/script.js
+++ js/script.js
@@ -3,47 +3,51 @@ var dataProvider = function () {
 
     var githubAPI = "https://api.github.com/users?access_token=3bceb316f870c6df7bca05a75a9c643822b18258";
 
-    $.getJSON(githubAPI, function (data) {
-    }).done(function(data) {
-        console.log("second success");
-        $('#userList').tmpl(data).appendTo('#main');
-    }).fail(function() {
-        console.log( "error" );
-    }).always(function() {
-        console.log( "complete" );
-    });
-
+    var GetListfUsers = function () {
+        $.getJSON(githubAPI, function (json) {
+        }).done(function(json) {
+            console.log("second success");
+            $('#userList').tmpl(json).appendTo('#main');
+        }).fail(function() {
+            console.log( "error" );
+        }).always(function() {
+            console.log( "complete" );
+        });
+    };
 
     var uploadMoreInfo = function (event) {
         event.preventDefault();
 
         var $button = $(this),
-            userLink = $button.attr('href'),
-            moreInfoContainer = $button.closest('.user-container').siblings('.user-moreContainer'),
-            infoList = moreInfoContainer.find('.more-info-test');
+            userLogin = $button.data('user-login'),
+            userLink =  "https://api.github.com/users/" + userLogin,
+            $userMoreInfoContainer = $button
+                .closest('.user-info')
+                .siblings('.user-more-info');
 
-        if(infoList.length) {
+        if($userMoreInfoContainer.has('.user-moreInfoItem').length) {
             return;
         }
 
-        $.getJSON( userLink , function ( data ) {
-            var items = [];
-
-            items.push(
-                "<li class='user-data-followers user-container--item'>Followers: " + data.followers + "</li>" +
-                "<li class='user-data-following user-container--item'>Following: " + data.following + "</li>");
-
-            $( "<ul/>", {
-                "class": "more-info-test",
-                html: items.join( "" )
-            }).appendTo(moreInfoContainer);
-
-        }).fail(function () {
-            alert('Json is empty');
+        $.getJSON(userLink, function (json) {
+        }).done(function(json) {
+            $('#userMoreInfo').tmpl(json).appendTo($userMoreInfoContainer);
+            $userMoreInfoContainer.find('.user-preloader').remove();
+        }).fail(function() {
+            console.log( "error" );
+        }).always(function() {
+            console.log( "complete" );
         });
-    }
+    };
 
-    $(document).on('click', '.js-more-button', uploadMoreInfo);
+    var init = function () {
+
+        GetListfUsers();
+
+        $(document).on('click', '.js-more-button', uploadMoreInfo);
+    };
+
+    init();
 
 };
 
